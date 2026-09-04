939c04bc0a02c17c34f65351829480b8bdde9b9d
Crevia <crevia.app@gmail.com>
2026-09-04 08:47:56 +0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'crevia',
   server: {
     port: 5173,
     host: true,
   },
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
   },
 });
